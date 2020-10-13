9711bb3b29d6cc8992679b83753adb65af57a56b
lrh2daizzo <herrera.lrh@gmail.com>
2020-10-13 10:27:01 -0500

    added new_from_collection constructor

--- lib/Hot_100/song.rb
+++ lib/Hot_100/song.rb
@@ -13,6 +13,7 @@ class Hot100::Song
     #constructor - make new songs from hot 100 chart
     def self.new_from_collection(c)
         Hot100::Song.new.tap do |song| c.each { |k,v| song.send("#{k}=", v)}
+            
         end
     end
 
